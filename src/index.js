c4bcd61789d61ba319ae6a06a384f63275742d97
rmnsnpk <69847180+rmnsnpk@users.noreply.github.com>
2021-02-04 22:26:14 +0300

    feat: end the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
+"use strict"
+
+
+
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length == 0) return 0;
+  if (array.length == 0 && Array.isArray(array)) {
+    return 0;
+  }
+  let minimal = array[0];
+  for(let i = 0; i<array.length; i++){
+    
+    if(minimal>array[i]){
+      minimal=array[i];
+    }
+  }
+  return minimal;
+  
 }
-
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length == 0) return 0;
+  if (array.length == 0 && Array.isArray(array)) {
+    return 0;
+  }
+  let maximum = array[0];
+  for(let i = 0; i<array.length ; i++){
+    
+    if(maximum<array[i]){
+      maximum=array[i];
+    }
+  }
+  return maximum;
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+if (arguments.length == 0) return 0;
+  if (array.length == 0 && Array.isArray(array)) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum / array.length;
 }
+
+
+// npm test
